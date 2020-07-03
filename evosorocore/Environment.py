fd8cc64067d907b5df41a8162b697489d48acf68
Jan Dolejs <dolejj13@fit.cvut.cz>
2020-07-03 13:52:54 +0200

    Cleaning up VXC

--- evosorocore/Environment.py
+++ evosorocore/Environment.py
@@ -61,19 +61,6 @@ class VXC_Wrapper(object):
 
         lattice = etree.SubElement(VXC_root, "Lattice")
         etree.SubElement(lattice, "Lattice_Dim").text = str(self.lattice_dimension)
-        #etree.SubElement(lattice, "X_Dim_Adj").text = "1"
-        #etree.SubElement(lattice, "Y_Dim_Adj").text = "1"
-        #etree.SubElement(lattice, "Z_Dim_Adj").text = "1"
-        #etree.SubElement(lattice, "X_Line_Offset").text = "0"
-        #etree.SubElement(lattice, "Y_Line_Offset").text = "0"
-        #etree.SubElement(lattice, "X_Layer_Offset").text = "0"
-        #etree.SubElement(lattice, "Y_Layer_Offset").text = "0"
-
-        #voxel = etree.SubElement(VXC_root, "Voxel")
-        #etree.SubElement(voxel, "Vox_Name").text = "BOX"
-        #etree.SubElement(voxel, "X_Squeeze").text = "1"
-        #etree.SubElement(voxel, "Y_Squeeze").text = "1"
-        #etree.SubElement(voxel, "Z_Squeeze").text = "1"
 
         if material_pallette != []:
             palette = etree.SubElement(VXC_root, "Palette")
