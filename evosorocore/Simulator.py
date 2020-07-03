fe718fd13171d97747646fb3beecb1e9d6c1e10f
Jan Dolejs <dolejj13@fit.cvut.cz>
2020-07-03 13:13:09 +0200

    Simulator update

--- evosorocore/Simulator.py
+++ evosorocore/Simulator.py
@@ -1,104 +1,95 @@
+#!/usr/bin/env python3
+
 from lxml import etree
 
+default_sim = \
+    {
+        #"FitnessFunction" : "UNDEFINED",
+        #0.0 ~ 1.0, how safe do we want to have the time step 
+        #1.0 if everything is fine, 0.5 if we want a safer run
+        "DtFrac" : 0.9,
+        #0.0 ~ 1.0, if we want voxels to jiggle a lot, we use 0.0, if we want to calm down, 1.0
+        "BondDampingZ" : 0.1,
+        #0.0 ~ 1.0, if we want voxels bouncing on the floor, we use 0.0, otherwise 1.0    
+        "ColDampingZ" : 1.0,
+        #0.0 ~ 1.0, if we want to dump everything in simulation, we use 1.0, otherwise 0.0
+        "SlowDampingZ" : 1.0, 
+        #number of seconds to run the experiment
+        "StopConditionFormula" : 1,
+        #number of steps per which we want to record voxels (e.g. 100 means every 100 steps)
+        "RecordStepSize" : 0,
+        #True or False, True if want to record voxels, else False
+        "RecordVoxel" : True,
+        #True or False, True if we want to record voxel links, else False
+        "RecordLink" : False,
+        #True or False, True if we want voxel collisions, else False
+        "EnableCollision" : True,
+        #"EnableAttach" : False,
+        #"AttachCondition" : "UNDEFINED",
+        #integer, number of steps in which there will be a special damping
+        "SafetyGuard" : 500,
+        #"ForceField" : "UNDEFINED",
+        #True or False, True if we want to enable signals, else False
+        "EnableSignals" : False,
+        #True or False, True if we want to enable Cilia, else False
+        "EnableCilia" : False,
+        #True or False, True if we want to save position of all voxels
+        "SavePositionOfAllVoxels" : False,
+        #real number, sometimes we need to count how many pairs of Target voxels are close to each other
+        "MaxDistInVoxelLengthsToCountAsPair" : 0
+    }
 
 class Sim(object):
-    """Container for VoxCad simulation parameters."""
+    """Container for voxcraft-sim simulation parameters."""
 
-    def __init__(self, self_collisions_enabled=True, simulation_time=10.5, dt_frac=0.9, stop_condition=2,
-                 fitness_eval_init_time=0.5, actuation_start_time=0, equilibrium_mode=0, min_temp_fact=0.1,
-                 max_temp_fact_change=0.00001, max_stiffness_change=10000, min_elastic_mod=5e006,
-                 max_elastic_mod=5e008, damp_evolved_stiffness=True, record_history=0,
-                 record_link=False):
-        self.sub_groups = ["Integration", "Damping", "Collisions", "Features", "StopCondition", "EquilibriumMode", "GA"]
-        # custom nested things in "SurfMesh", "CMesh"
+    def __init__(self, parameters=default_sim):
 
-        self.self_collisions_enabled = self_collisions_enabled
-        self.simulation_time = simulation_time
-        self.dt_frac = dt_frac
-        self.stop_condition = stop_condition
-        self.fitness_eval_init_time = fitness_eval_init_time
-        self.actuation_start_time = actuation_start_time
-        self.equilibrium_mode = equilibrium_mode
-        self.min_temp_fact = min_temp_fact
-        self.max_temp_fact_change = max_temp_fact_change
-        self.max_stiffness_change = max_stiffness_change
-        self.min_elastic_mod = min_elastic_mod
-        self.max_elastic_mod = max_elastic_mod
-        self.damp_evolved_stiffness = damp_evolved_stiffness
-        self.record_history = record_history
-        self.record_link = record_link
+        self.DtFrac = parameters["DtFrac"]
+        self.BondDampingZ = parameters["BondDampingZ"]
+        self.ColDampingZ = parameters["ColDampingZ"]
+        self.SlowDampingZ = parameters["SlowDampingZ"]
+        self.StopConditionFormula = parameters["StopConditionFormula"]
+        self.RecordStepSize = parameters["RecordStepSize"]
+        self.RecordVoxel = 1 if parameters["RecordVoxel"] else 0
+        self.RecordLink = 1 if parameters["RecordLink"] else 0
+        self.EnableCollision = 1 if parameters["EnableCollision"] else 0
+        self.SafetyGuard = parameters["SafetyGuard"]
+        self.EnableSignals = 1 if parameters["EnableSignals"] else 0
+        self.EnableCilia = 1 if parameters["EnableCilia"] else 0
+        self.SavePositionOfAllVoxels = 1 if parameters["SavePositionOfAllVoxels"] else 0
+        self.MaxDistInVoxelLengthsToCountAsPair = parameters["MaxDistInVoxelLengthsToCountAsPair"]
 
     def write_to_xml(self, root, run_dir=None, individual=None, fitness_file_str=None, seed=0, **kwargs):
         sim_root = etree.SubElement(root, "Simulator")
 
-        #etree.SubElement(sim_root, "Seed").text = str(seed)
+        integration = etree.SubElement(sim_root, "Integration")
+        etree.SubElement(integration, "DtFrac").text = str(self.DtFrac)
 
-        #integration = etree.SubElement(sim_root, "Integration")
-        #etree.SubElement(integration, "Integrator").text = "0"
-        #etree.SubElement(integration, "DtFrac").text = str(self.dt_frac)
-
-        #damping = etree.SubElement(sim_root, "Damping")
-        #etree.SubElement(damping, "BondDampingZ").text = "1"
-        #etree.SubElement(damping, "ColDampingZ").text = "0.8"
-        #etree.SubElement(damping, "SlowDampingZ").text = "0.01"
-
-        #collisions = etree.SubElement(sim_root, "Collisions")
-        #etree.SubElement(collisions, "SelfColEnabled").text = str(int(self.self_collisions_enabled))
-        #etree.SubElement(collisions, "ColSystem").text = "3"
-        #etree.SubElement(collisions, "CollisionHorizion").text = "2"
-
-        #features = etree.SubElement(sim_root, "Features")
-        #etree.SubElement(features, "FluidDampEnabled").text = "0"
-        #etree.SubElement(features, "PoissonKickBackEnabled").text = "0"
-        #etree.SubElement(features, "EnforceLatticeEnabled").text = "0"
-
-        #surf_mesh = etree.SubElement(sim_root, "SurfMesh")
-        #c_mesh = etree.SubElement(surf_mesh, "CMesh")
-        #etree.SubElement(c_mesh, "DrawSmooth").text = "1"
-        #etree.SubElement(c_mesh, "Vertices")
-        #etree.SubElement(c_mesh, "Facets")
-        #etree.SubElement(c_mesh, "Lines")
-
-        record_history = etree.SubElement(sim_root, "RecordHistory")
-        etree.SubElement(record_history, "RecordStepSize").text = str(int(self.record_history))
-        etree.SubElement(record_history, "RecordVoxel").text = "1" if self.record_history else "0"
-        etree.SubElement(record_history, "RecordLink").text = "1" if self.record_link else "0"
+        damping = etree.SubElement(damping, "Damping")
+        etree.SubElement(damping, "BondDampingZ").text = str(self.BondDampingZ)
+        etree.SubElement(damping, "ColDampingZ").text = str(self.ColDampingZ)
+        etree.SubElement(damping, "SlowDampingZ").text = str(self.SlowDampingZ)
 
         stop_condition = etree.SubElement(sim_root, "StopCondition")
         stop_condition_formula = etree.SubElement(stop_condition, "StopConditionFormula")
         mtsub = etree.SubElement(stop_condition_formula, "mtSUB")
         etree.SubElement(mtsub, "mtVAR").text = "t"
-        etree.SubElement(mtsub, "mtCONST").text = str(int(self.stop_condition))
+        etree.SubElement(mtsub, "mtCONST").text = str(self.StopConditionFormula)
+       
+        record_history = etree.SubElement(sim_root, "RecordHistory")
+        etree.SubElement(record_history, "RecordStepSize").text = str(self.RecordStepSize)
+        etree.SubElement(record_history, "RecordVoxel").text = str(self.RecordVoxel) 
+        etree.SubElement(record_history, "RecordLink").text = str(self.RecordLink)
 
-        #equilibrium_mode = etree.SubElement(sim_root, "EquilibriumMode")
-        #etree.SubElement(equilibrium_mode, "EquilibriumModeEnabled").text = str(self.equilibrium_mode)
+        attachdetach = etree.SubElement(sim_root, "AttachDetach")
+        etree.SubElement(attachdetach, "EnableCollision").text = str(self.EnableCollision)
+        etree.SubElement(attachdetach, "SafetyGuard").text = str(self.SafetyGuard)
 
-        #G_A = etree.SubElement(sim_root, "GA")
-        #etree.SubElement(G_A, "WriteFitnessFile").text = "1"
-        #if fitness_file_str is not None:
-        #    etree.SubElement(G_A, "FitnessFileName").text = fitness_file_str
-        #else:
-        #    etree.SubElement(G_A, "FitnessFileName").text = "%s/fitnessFiles/softbotsOutput--id_%05i.xml" % (run_dir, individual.get_id())
-        #etree.SubElement(G_A, "QhullTmpFile").text = "%s/../_qhull/tempFiles/qhullInput--id_%05i.txt" % (run_dir, individual.get_id())
-        #etree.SubElement(G_A, "CurvaturesTmpFile").text = "%s/../_qhull/tempFiles/curvatures--id_%05i.txt" % (run_dir, individual.get_id())
-
-        #etree.SubElement(sim_root, "MinTempFact").text = str(self.min_temp_fact)
-        #etree.SubElement(sim_root, "MaxTempFactChange").text = str(self.max_temp_fact_change)
-        #etree.SubElement(sim_root, "DampEvolvedStiffness").text = str(int(self.damp_evolved_stiffness))
-        #etree.SubElement(sim_root, "MaxStiffnessChange").text = str(self.max_stiffness_change)
-        #etree.SubElement(sim_root, "MinElasticMod").text = str(self.min_elastic_mod)
-        #etree.SubElement(sim_root, "MaxElasticMod").text = str(self.max_elastic_mod)
-        #etree.SubElement(sim_root, "ErrorThreshold").text = str(0)
-        #etree.SubElement(sim_root, "ThresholdTime").text = str(0)
-        #etree.SubElement(sim_root, "MaxKP").text = str(0)
-        #etree.SubElement(sim_root, "MaxKI").text = str(0)
-        #etree.SubElement(sim_root, "MaxANTIWINDUP").text = str(0)
-
-        #if hasattr(individual, "parent_lifetime"):
-        #    if individual.parent_lifetime > 0:
-        #        etree.SubElement(sim_root, "ParentLifetime").text = str(individual.parent_lifetime)
-        #    else:
-        #        etree.SubElement(sim_root, "ParentLifetime").text = str(individual.lifetime)
-
+        etree.SubElement(sim_root, "EnableSignals").text = str(self.EnableSignals)
+        etree.SubElement(sim_root, "EnableCilia").text = str(self.EnableCilia)
+        etree.SubElement(sim_root, "SavePositionOfAllVoxels").text = str(self.SavePositionOfAllVoxels)
+        etree.SubElement(sim_root, "MaxDistInVoxelLengthsToCountAsPair").text = str(self.MaxDistInVoxelLengthsToCountAsPair)
+        
+            
 
         return sim_root
