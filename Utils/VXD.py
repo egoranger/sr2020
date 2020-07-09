35b7d0d280863d53b8ff8762d5fe721bb8b3b68d
Jan Dolejs <dolejj13@fit.cvut.cz>
2020-07-09 13:40:46 +0200

    Moving VXA -> VXD to VXD.py

--- Utils/VXD.py
+++ Utils/VXD.py
@@ -2,6 +2,7 @@
 
 from evosorocore2.Robot import Robot
 from lxml import etree
+import numpy as np
 
 class VXD( object ):
   """
@@ -22,9 +23,70 @@ class VXD( object ):
   def create_header( self ):
     self.root = etree.Element( "VXD" )
 
-  #TODO overwrite robot if needed?
-  #TODO import info from VXA file?
+  def convert_structure( self, structure ):
+    """
+    @input: etree element with structure data from vxa file
+    @output: numpy array
+    """
+    #get dimensions
+    x = int( structure.find( "X_Voxels" ).text )
+    y = int( structure.find( "Y_Voxels" ).text )
+    z = int( structure.find( "Z_Voxels" ).text )
 
+    
+    data = structure.find( "Data" )
+
+    #extract layers from data
+    assert z == len( data ), "z dimension of the file is incorrect"
+    z_list = []
+    for layer in data:
+      assert len( layer.text ) == x * y, "x & y dimensions are incorrect"
+      y_list = []
+      for i in range( y ):
+        x_list = []
+        for j in range( x ):
+          x_list.append( int( layer.text[i*x + j] ) )
+        y_list.append( x_list )
+      z_list.append( y_list )
+
+    return np.array( z_list )
+
+  def minimize_arr( self, arr ):
+    """
+    @input: numpy array
+    @output: minimized numpy array
+    Minimize number of materials and get rid of unwanted zeros.
+    """
+    ind = np.nonzero( arr )
+
+    #created minimized array using
+    min_arr =  arr[ind[0][0]:ind[0][-1] + 1, ind[1][0]:ind[1][-1] + 1, ind[2][0]:ind[2][-1] + 1]
+    #minimize the number of used materials
+    unq = np.unique( min_arr )
+    for i,u in zip( range( 1, len( unq ) + 1 ), unq ):
+      if i != u:
+        min_arr[np.where( min_arr == u )] -= u - i
+ 
+    return min_arr
+
+  def create_bot_from_vxa( self, filename ):
+    try:
+      root = etree.parse( filename )
+    except OSError:
+      print("An error occured! Perhaps filename is wrong?")
+      return False
+    except:
+      print("An unknown error occured! Please try again.")
+      return False
+
+    strut = root.find( "*/Structure" )
+
+    arr = self.convert_structure( strut )
+    min_arr = self.minimize_arr( arr )
+    self.robot = Robot( min_arr )
+
+    return True
+     
 #run some tests
 if __name__ == "__main__":
   vxd = VXD()
--- evosorocore2/Robot.py
+++ evosorocore2/Robot.py
@@ -14,45 +14,18 @@ class Robot(object):
             self.morphology = morphology
         else:
             self.morphology = np.ones(shape=(2, 2, 5), dtype=np.int8)
-            self.mutate()
-
-    def get_body_xlim(self):
-        return self.morphology.shape[0]
-
-    def get_body_ylim(self):
-        return self.morphology.shape[1]
-
-    def get_body_zlim(self):
-        return self.morphology.shape[2]
-
-    def mutate(self):
-        """
-        very simple mutation.
-        :return:
-        """
-        # mutate one voxel: flip it's bit
-        x = random.randint(0, self.morphology.shape[0] - 1)
-        y = random.randint(0, self.morphology.shape[1] - 1)
-        z = random.randint(0, self.morphology.shape[2] - 1)
-        if self.morphology[x, y, z]:
-            self.morphology[x, y, z] = 0
-        else:
-            self.morphology[x, y, z] = 1
 
     def write_to_xml(self, root, **kwargs):
         structure = etree.SubElement(root, "Structure")
         structure.set('replace', 'VXA.VXC.Structure')
         structure.set('Compression', 'ASCII_READABLE')
 
-        etree.SubElement(structure, "X_Voxels").text = str(self.morphology.shape[0])
+        etree.SubElement(structure, "X_Voxels").text = str(self.morphology.shape[2])
         etree.SubElement(structure, "Y_Voxels").text = str(self.morphology.shape[1])
-        etree.SubElement(structure, "Z_Voxels").text = str(self.morphology.shape[2])
-        #etree.SubElement(structure, "numRegenerationModelSynapses").text = "0"
-        #etree.SubElement(structure, "numForwardModelSynapses").text = "0"
-        #etree.SubElement(structure, "numControllerSynapses").text = "0"
+        etree.SubElement(structure, "Z_Voxels").text = str(self.morphology.shape[0])
 
         data = etree.SubElement(structure, "Data")
-        for layer in range(self.morphology.shape[2]):
-            etree.SubElement(data, "Layer").text = etree.CDATA(''.join([str(d) for d in self.morphology[:, :, layer].flatten()]))
+        for layer in range(self.morphology.shape[0]):
+            etree.SubElement(data, "Layer").text = etree.CDATA(''.join([str(d) for d in self.morphology[layer, :, :].flatten()]))
 
         return structure
