30b449abe8f8f070e6a84ed9d7759b82694321e0
Jan Dolejs <dolejj13@fit.cvut.cz>
2020-07-14 10:26:05 +0200

    Adding time seed for random and np.random

--- experiment.py
+++ experiment.py
@@ -4,15 +4,21 @@ from evosorocore2.Simulator import default_sim
 from evosorocore2.Environment import default_env
 from Utils.fitness import Distance
 from Utils.VXA import VXA
-from Utils.tools import create_folders
+from Utils.tools import create_folders,file_stream_handler
 from Simulation_Manager import SimulationManager as SM
 import map_elites.cvt as cvt_map_elites
 import map_elites.common as cm_map_elites
 import numpy as np
+import random
+import time
+import logging
 
 if __name__ == "__main__":
 
-  #TODO seeds
+  seed = int( time.time() )
+  
+  random.seed( seed )
+  np.random.seed( seed )
 
   number_of_materials = 3
   mult_arr = np.array( [ 1e6, 5, 1, 1.5e3, 0.1,
@@ -24,6 +30,16 @@ if __name__ == "__main__":
 
   #create experiment folders
   dirs = create_folders( exp_folder )
+
+  #create logger
+  logger = logging.getLogger( __name__ )
+  f,s = file_stream_handler( dirs["experiment"] + "/" + logfile )
+  logger.addHandler( f )
+  logger.addHandler( s )
+  logger.setLevel( logging.DEBUG )
+
+  #save seed
+  logger.info( "Using seed: {0}".format( seed ) )
 
   #simulator and environment parameters
   sim = default_sim.copy()
@@ -47,6 +63,7 @@ if __name__ == "__main__":
   px["batch_size"] = 10
   px["random_init_batch"] = 10
 
+  logger.info("Running Map Elites now")
   #TODO dim_x depends on # of material properties
   cvt_map_elites.compute( 2, 5*number_of_materials, simulation.fitness,
                           n_niches=100, max_evals=1000, 
